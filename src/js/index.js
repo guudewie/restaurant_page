d0ec6278d3d7696aa5d58387958a3359d59310a6
guudewie <philippthiessen12@gmail.com>
2023-07-16 18:48:37 +0200

    add function to open menu page

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-import { openWebsiteFrame } from '../js/website/website.js'
-import { openHome } from './home/home.js'
-import { openAboutUs } from './aboutus/aboutus.js';
+import { openWebsiteFrame } from '../js/website/website.js';
+//import { openHome } from './home/home.js';
+import { openMenu } from './menu/menu.js';
+//import { openAboutUs } from './aboutus/aboutus.js';
 
 
 window.addEventListener("load", () => {
     openWebsiteFrame();
-    openAboutUs();
-    openHome()
+    openMenu();
 })
 
 // import openMenu() from menu
--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -1,7 +1,93 @@
-// export openMenu()
+import { container } from "webpack"
 
+export function openMenu() {
+    
+    const mainAnker = document.querySelector(".main")
 
-function openMenu() {
-    // append divs to div
-    // add classes
+    // create menu page container
+    const menuPage = document.createElement("div")
+    menuPage.setAttribute("id", "menu-page")
+
+    mainAnker.appendChild(menuPage)
+
+    // create menu containers
+    const containerTT = document.createElement("div")
+    containerTT.classList.add("pizza-container")
+    mainAnker.appendChild(containerTT)
+
+    const containerFF = document.createElement("div")
+    containerFF.classList.add("pizza-container")
+    mainAnker.appendChild(containerFF)
+
+    const containerHH = document.createElement("div")
+    containerHH.classList.add("pizza-container")
+    mainAnker.appendChild(containerHH)
+
+    const containerOO = document.createElement("div")
+    containerOO.classList.add("pizza-container")
+    mainAnker.appendChild(containerOO)
+
+    const containerSS = document.createElement("div")
+    containerSS.classList.add("pizza-container")
+    mainAnker.appendChild(containerSS)
+
+    const containerVV = document.createElement("div")
+    containerVV.classList.add("pizza-container")
+    mainAnker.appendChild(containerVV)
+
+    //add images
+    const iconTT = document.createElement("img")
+    iconTT.setAttribute("src", "../src/assets/pizza_bacon.png")
+    containerTT.appendChild(iconTT)
+    
+    const iconFF = document.createElement("img")
+    iconFF.setAttribute("src", "../src/assets/pizza_funghi.png")
+    containerFF.appendChild(iconFF)
+    
+    const iconHH = document.createElement("img")
+    iconHH.setAttribute("src", "../src/assets/pizza_hawaii.png")
+    containerHH.appendChild(iconHH)
+    
+    const iconOO = document.createElement("img")
+    iconOO.setAttribute("src", "../src/assets/pizza_olive.png")
+    containerOO.appendChild(iconOO)
+    
+    const iconSS = document.createElement("img")
+    iconSS.setAttribute("src", "../src/assets/pizza_salami.png")
+    containerSS.appendChild(iconSS)
+    
+    const iconVV = document.createElement("img")
+    iconVV.setAttribute("src", "../src/assets/pizza_veggie.png")
+    containerVV.appendChild(iconVV)
+
+    //add title
+    const textTT = document.createElement("div")
+    textTT.classList.add("pizza-text")
+    textTT.innerHTML = "Talented Tomato"
+    containerTT.appendChild(textTT)
+
+    const textFF = document.createElement("div")
+    textFF.classList.add("pizza-text")
+    textFF.innerHTML = "Fantastic Funghi"
+    containerFF.appendChild(textFF)
+
+    const textHH = document.createElement("div")
+    textHH.classList.add("pizza-text")
+    textHH.innerHTML = "Hawaiin Hawai"
+    containerHH.appendChild(textHH)
+
+    const textOO = document.createElement("div")
+    textOO.classList.add("pizza-text")
+    textOO.innerHTML = "Owesome Olive"
+    containerOO.appendChild(textOO)
+
+    const textSS = document.createElement("div")
+    textSS.classList.add("pizza-text")
+    textSS.innerHTML = "Owesome Olive"
+    containerSS.appendChild(textSS)
+
+    const textVV = document.createElement("div")
+    textVV.classList.add("pizza-text")
+    textVV.innerHTML = "Veggetarian Veggie"
+    containerVV.appendChild(textVV)
 }
